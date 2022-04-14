f0ad3200562c4a041186f832c27ca6a5f8c85902
Fabian Ekbäck <fabian.ekback@gmail.com>
2022-04-14 10:50:14 +0200

    Initial commit

---
+++ src/main/java/com/lubian/grupparbete/LubianApplication.java
@@ -0,0 +1,13 @@
+package com.lubian.grupparbete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LubianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LubianApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lubian/grupparbete/LubianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lubian.grupparbete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LubianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
